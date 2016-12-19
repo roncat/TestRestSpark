d4a38fc076ef04fde9e0e35bdc69e2dd6c1c6171
flavio.gomes <flavio.gomes@tse.jus.br>
2016-12-19 21:05:19 -0200

    Updated context name

--- src/main/java/TestRest.java
+++ src/main/java/TestRest.java
@@ -2,7 +2,7 @@ import static spark.Spark.*;
 
 public class TestRest {
     public static void main(String[] args) {
-       get("/rest", (req, res) -> "Olá Mundo!");
+       get("/ola", (req, res) -> "Olá Mundo!");
        get("/hello", (req, res) -> "Hello World");
     }
 }
